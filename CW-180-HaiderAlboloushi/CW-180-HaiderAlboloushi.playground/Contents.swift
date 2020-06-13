5dea04a0dbf15910f2e161497baf0cf5676abd90
HaiderAlboloushi180 <halbolou05@gmail.com>
2020-06-13 20:02:16 +0300

    added work

--- CW-180-HaiderAlboloushi/CW-180-HaiderAlboloushi.playground/Contents.swift
+++ CW-180-HaiderAlboloushi/CW-180-HaiderAlboloushi.playground/Contents.swift
@@ -10,6 +10,28 @@ var hobby2 = "martial arts"
 
 var age = 15
 
-var height = 164.5
+var height = 164
 
 print("Hello my name is \(name), I am \(age) and I am \(height)cm tall, I like \(hobby1) and \(hobby2)")
+
+let friendName = "Khaled Abdulhadi"
+
+var friendHeight = 160
+
+var friendAge = 15
+
+var friendHobby1 = "basketball"
+
+var friendHobby2 = "gaming"
+
+var age_difference = age - friendAge
+
+name != friendName
+
+age == friendAge
+
+height > friendHeight
+
+hobby1 == friendHobby2
+
+hobby2 != friendHobby1
