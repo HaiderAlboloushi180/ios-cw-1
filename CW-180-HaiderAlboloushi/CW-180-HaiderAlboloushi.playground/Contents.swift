d92ec4f406de8d0723046c8ee4793b5dba8d0e6c
HaiderAlboloushi180 <halbolou05@gmail.com>
2020-06-13 19:08:25 +0300

    Finished CW

---
+++ CW-180-HaiderAlboloushi/CW-180-HaiderAlboloushi.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "Haider Alboloushi"
+
+var hobby1 = "gaming"
+
+var hobby2 = "martial arts"
+
+var age = 15
+
+var height = 164.5
+
+print("Hello my name is \(name), I am \(age) and I am \(height)cm tall, I like \(hobby1) and \(hobby2)")
